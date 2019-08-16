3415b49cdfba6103fd266840d2d8a865bd512ae2
Atilla Barros <atilla.barros@gamesys.co.uk>
2019-08-16 11:25:06 +0100

    Project setup; initial commit with maven dependencies;

---
+++ src/main/java/com/revolut/ibank/IbankApplication.java
@@ -0,0 +1,13 @@
+package com.revolut.ibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revolut/ibank/IbankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revolut.ibank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
