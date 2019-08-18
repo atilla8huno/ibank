a897a81f6a8a611af01a7fb8957709a1a1f3e1d7
Atilla Barros <atilla.barros@gamesys.co.uk>
2019-08-18 14:11:04 +0100

    Renamed mapper interface

--- src/main/java/com/revolut/ibank/mapper/AccountMapper.java
+++ src/main/java/com/revolut/ibank/mapper/AccountMapper.java
@@ -2,19 +2,8 @@ package com.revolut.ibank.mapper;
 
 import com.revolut.ibank.data.entity.AccountEntity;
 import com.revolut.ibank.domain.PersonalAccount;
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
-import org.springframework.stereotype.Component;
 
-@Component
-@AllArgsConstructor
-public class AccountMapper implements AccountEntityToDomainMapper {
+public interface AccountEntityToDomainMapper {
 
-    @Override
-    public PersonalAccount mapToPersonalAccount(@NonNull AccountEntity accountEntity) {
-        return new PersonalAccount(
-                accountEntity.getName(),
-                accountEntity.getAccountNumber(),
-                accountEntity.getBalance());
-    }
+    PersonalAccount mapToPersonalAccount(AccountEntity accountEntity);
 }
---
+++ src/main/java/com/revolut/ibank/mapper/AccountMapperImpl.java
@@ -0,0 +1,20 @@
+package com.revolut.ibank.mapper;
+
+import com.revolut.ibank.data.entity.AccountEntity;
+import com.revolut.ibank.domain.PersonalAccount;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class AccountMapper implements AccountEntityToDomainMapper {
+
+    @Override
+    public PersonalAccount mapToPersonalAccount(@NonNull AccountEntity accountEntity) {
+        return new PersonalAccount(
+                accountEntity.getName(),
+                accountEntity.getAccountNumber(),
+                accountEntity.getBalance());
+    }
+}
